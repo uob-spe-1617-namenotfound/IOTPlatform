e661511775d7abf9dc6aa1899bf7f5b3e9e7a773
MarahMohFaroun <marah.faroun@yahoo.com>
2016-11-14 16:10:29 +0000

    update Devices and Homeactions.

--- website/views/__init__.py
+++ website/views/__init__.py
@@ -2,9 +2,11 @@ from flask import render_template
 
 from website import app
 
-@app.route('/Triggers')
+
+@app.route('/triggers')
 def triggers():
-        return render_template("Triggers.html")
+    return render_template("triggers.html")
+
 
 @app.route('/')
 def index():
@@ -20,10 +22,12 @@ def logout():
 def account_settings():
     return "To be implemented"
 
+
 @app.route('/Home Actions')
 def home_actions():
     return render_template("homeactions.html")
 
+
 @app.route('/Devices')
 def devices():
-    return render_template("Devices.html")
+    return render_template("devices.html")
