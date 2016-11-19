64698a5989ac46bfd24fb95bc591dc492f16cc4e
MarahMohFaroun <marah.faroun@yahoo.com>
2016-11-19 00:35:38 +0000

    Tried to make the page more specific (for a motion sensor).

--- website/navbar.py
+++ website/navbar.py
@@ -7,7 +7,7 @@ navbar = Navbar(
              View('Settings', '.account_settings'),
              View('Log out', '.logout')
              ),
-    View('Home Actions', '.home_actions'),
+    View('Device Actions', '.device_actions'),
     View('Devices', '.devices'),
     View('Room View', '.room_view')
 )
