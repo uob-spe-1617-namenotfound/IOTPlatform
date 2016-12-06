422af585003987c4227b57b0e58dfb3b3183e5a8
Benny Clark <Benny Clark>
2016-12-06 13:16:46 +0000

    13:16 on 06/12 (third commit)

--- data_model/model.py
+++ data_model/model.py
@@ -18,8 +18,9 @@ class UserRepository:
     def __init__(self):
         self.users = {}
 
-    def add_user(self, user_id):
-        self.users[user_id] = user_id
+    def add_user(self, user):
+        self.users[user.id] = user
+        self.users.update({'id': user})
 
     def remove_user(self, user_id):
         self.users.pop(user_id, None)
@@ -47,22 +48,22 @@ class HouseRepository:
         self.houses = {}
         self.house_groups = {}
 
-    def add_house(self, house_id):
-        self.houses[house_id] = house_id
+    def add_house(self, house):
+        self.houses[house.id] = house
+        self.houses.update({'id': house})
 
     def add_house_group(self, house_group):
         self.house_groups[house_group.id] = house_group
+        self.house_groups.update({'id': house_group})
 
-    def add_house_to_group(self, house, house_group):
-        # If this method is called multiple times for a specific group, would the first line reset the group to []?
-        self.house_groups[house_group.id] = []
-        self.house_groups[house_group.id] += self.house_groups[house.id]
+    def add_house_to_group(self, house_group):
+        self.house_groups[house_group.id] += self.house_groups
 
     def remove_house(self, house_id):
         self.houses.pop(house_id, None)
 
-    def remove_house_group(self, house_group):
-        self.house_groups.pop(house_group.id, None)
+    def remove_house_group(self, house_group_id):
+        self.house_groups.pop(house_group_id, None)
 
     def get_house_by_id(self, house_id):
         try:
@@ -71,9 +72,7 @@ class HouseRepository:
             print("House %d not found" % house_id)
 
     def add_house_to_user(self, user, house_id):
-        # self.users[user.id] = []
-        # self.users[user.id] += HouseRepository.users[user.id]
-        user += house_id
+        user += self.houses[house_id]
 
     def get_houses_for_user(self, user_id):
         for house in self.houses:
@@ -98,18 +97,18 @@ class RoomRepository:
 
     #    self.room_groups = {}
 
-    def add_room(self, room_id):
-        self.rooms[room_id] = room_id
+    def add_room(self, room):
+        self.rooms[room.id] = room
+        self.rooms.update({'id': room})
 
     # def add_room_group(self, room_group):
     #    self.room_groups[room_group.id] = room_group
 
     # def add_room_to_group(self, room, room_group):
-    #    self.room_groups[room_group.id] = []
     #    self.room_groups[room_group.id] += self.rooms[room.id]
 
-    def remove_room(self, room):
-        self.rooms.pop(room.id, None)
+    def remove_room(self, room_id):
+        self.rooms.pop(room_id, None)
 
     def get_room_by_id(self, room_id):
         try:
@@ -117,18 +116,18 @@ class RoomRepository:
         except KeyError:
             print("Room %d not found" % room_id)
 
-    def get_devices_for_room(self, room):
-        return self.rooms[room.id]
+    def get_devices_for_room(self, devices, room_id):
+        for device in devices:
+            if device.room_id == room_id:
+                return device
 
     def add_room_to_house(self, house, room_id):
         house += self.rooms[room_id]
 
-    def get_rooms_for_house(self, house):
-        room = []
-        for elem in house:
-            if elem in self.rooms:
-                room += []
-        return room
+    def get_rooms_for_house(self, rooms, house_id):
+        for room in rooms:
+            if room.house_id == house_id:
+                return room
 
 
 class Device(House, Room):
@@ -140,15 +139,15 @@ class Device(House, Room):
         self.power_state = power_state
 
     def get_device_attributes(self):
-        return {'house_id': self.house_id, 'room_id' self.room_id, 'device_id': self.device_id, 'name': self.name,
+        return {'house_id': self.house_id, 'room_id': self.room_id, 'device_id': self.device_id, 'name': self.name,
                 'power_state': self.power_state}
 
     def change_power_state(self):
         try:
             if self.power_state == 1:
-                power_state = 0
+                self.power_state = 0
             elif self.power_state == 0:
-                power_state = 1
+                self.power_state = 1
         except:
             return "ERROR: Power State not 1 or 0"
 
@@ -157,8 +156,8 @@ class DeviceRepository:
     def __init__(self):
         self.devices = {}
 
-    def add_device_to_devices(self, device_id):
-        self.devices[device_id] = device_id
+    def add_device_to_devices(self, device):
+        self.devices[device.id] = device
 
     def remove_device(self, device_id):
         self.devices.pop(device_id, None)
@@ -172,22 +171,25 @@ class DeviceRepository:
     def add_device_to_house(self, house, device_id):
         house += self.devices[device_id]
 
-    def get_devices_for_house(self, house):
-        devs = []
-        for elem in house:
-            if elem in self.devices:
-                devs += elem
-        return devs
+    def get_devices_for_house(self, devices, house_id):
+        for device in devices:
+            if device.house_id == house_id:
+                return device
 
     def add_device_to_room(self, room, device_id):
-        room += device_id
+        room += self.devices[device_id]
+
+    def get_devices_for_room(self, devices, room_id):
+        for device in devices:
+            if device.room_id == room_id:
+                return device
 
 
 class DeviceGroups(Device):
     def __init__(self, device_id):
         self.device_id = device_id
 
-    def get_devicegroup_attributes(self):
+    def get_device_group_attributes(self):
         return {'device_id': self.device_id}
 
 
