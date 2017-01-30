21fb6ed0be6a5ecd2ecb8d323935ec0646789539
Benny Clark <Benny Clark>
2017-01-30 16:58:08 +0000

    updated cron + 'faulty thermostat' with ben's changes

--- api/last_temp.py
+++ api/last_temp.py
@@ -3,14 +3,27 @@ from data_model import model
 from data_model.tempcron import schedule_cron_job
 
 
-def get_last_temp():
-    r = requests.get("http://localhost:5010/read")
+# def get_last_temp():
+#     r = requests.get("http://localhost:5010/read")
+#     x = r.json().get('data')
+#     thermostat = model.Device(1, 2, 3, "Benny's Thermostat", "thermostat", 1, 25.0, 20, 0)
+#     print("last temperature = %.2f" % thermostat.last_temp)
+#     temp = x.get('temperature')
+#     thermostat.last_temp = temp
+#
+#     print("current temperature = %.2f" % thermostat.last_temp)
+
+def get_last_timestamp():
+    r = requests.get("http://localhost:5010/thermostat/1")
     x = r.json().get('data')
-    thermostat = model.Device(1, 2, 3, "Benny's Thermostat", "thermostat", 1, 25.0, 20, 0)
-    print("last temperature = %.2f" % thermostat.last_temp)
-    temp = x.get('temperature')
-    thermostat.last_temp = temp
-
-    print("current temperature = %.2f" % thermostat.last_temp)
-
-schedule_cron_job()
+    time = x.get('timestamp')
+    resolution = x.get('resolution')
+    timestamp1 = "Wed, 25 Jan 2017 17:15:42 GMT"
+    t1 = datetime.strptime(timestamp1, "%a, %d %b %Y %H:%M:%S %Z")
+    t2 = datetime.strptime(time, "%a, %d %b %Y %H:%M:%S %Z")
+    print(timestamp1)
+    print(time)
+    difference = t2 - t1
+    print(difference.seconds)
+    if difference.seconds > resolution:
+        print("DEVICE BROKEN")
--- api/tempcron.py
+++ api/tempcron.py
@@ -1,4 +1,4 @@
-from data_model.last_temp import get_last_temp
+from data_model.last_temp import get_last_timestamp
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.interval import IntervalTrigger
 
@@ -8,9 +8,10 @@ scheduler.start()
 
 def schedule_cron_job():
     scheduler.add_job(
-        func=get_last_temp,
+        func=get_last_timestamp,
         trigger=IntervalTrigger(seconds=2),
         id='get_last_temp',
-        name='Get last temperature for dummy thermostat',
+        name='Get last timestamp for the faulty thermostat',
         replace_existing=True)
 
+schedule_cron_job()
