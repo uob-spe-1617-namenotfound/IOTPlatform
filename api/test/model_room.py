c50740a72418be764724a8fd4002ac856332be7c
ben fossett <ben fossett>
2017-02-20 13:12:57 +0000

    Updated with house, room, and device tests as well as appropriate changes to model and repositories.

--- api/test/model_room.py
+++ api/test/model_room.py
@@ -1,8 +1,36 @@
 from api import repositories, model
 import unittest
+from bson import ObjectId
 
 
 class RoomTests(unittest.TestCase):
     def __init__(self, testName):
         unittest.TestCase.__init__(self, testName)
         self.rooms = repositories.RoomRepository(RoomTests.collection)
+        self.house1id = ObjectId()
+        self.room1id = self.rooms.add_room(self.house1id, "Living Room")
+        self.room2id = self.rooms.add_room(self.house1id, "Kitchen")
+        self.room3id = self.rooms.add_room(self.house1id, "Bathroom")
+
+    def test_RoomAddedCorrectly(self):
+        room3 = self.rooms.get_room_by_id(self.room3id)
+        house_id = room3.get_room_house()
+        self.assertEqual(house_id, self.house1id, "Room house not added correctly.")
+        name = room3.get_room_name()
+        self.assertEqual(name, "Bathroom", "Room name not added correctly.")
+
+    def test_GetRoomsForHouse(self):
+        rooms = self.rooms.get_rooms_for_house(self.house1id)
+        self.assertEqual(len(rooms), 3, "Incorrect amount of rooms.")
+        name1 = rooms[0].get_room_name()
+        name2 = rooms[1].get_room_name()
+        name3 = rooms[2].get_room_name()
+        self.assertEqual(name1, "Living Room", "First room has incorrect name.")
+        self.assertEqual(name2, "Kitchen", "Second room has incorrect name.")
+        self.assertEqual(name3, "Bathroom", "Third room has incorrect name.")
+
+    def test_RoomRemovedCorrectly(self):
+        all_rooms = self.rooms.get_all_rooms()
+        self.rooms.remove_room(self.room3id)
+        all_remaining_rooms = self.rooms.get_all_rooms()
+        self.assertEqual(len(all_remaining_rooms), len(all_rooms) - 1, "Incorrect number of remaining rooms.")
--- api/tests.py
+++ api/tests.py
@@ -2,19 +2,22 @@ from pymongo import MongoClient
 from test.model_user import UserTests
 from test.model_house import HouseTests
 from test.model_room import RoomTests
+from test.model_device import DeviceTests
 import unittest
 
 mongo = MongoClient('localhost', 27017)
-db = mongo.database
+db = mongo.testdb
 
 def main():
     #suite = unittest.TestSuite()
     #suite.addTest(UserTests(db.users_tests))
     #suite = unittest.defaultTestLoader.loadTestsFromTestCase(UserTests(db.users_test))
     #unittest.TextTestRunner().run(suite)
+    mongo.drop_database('testdb')
     UserTests.collection = db.user_test
     HouseTests.collection = db.house_test
     RoomTests.collection = db.room_test
+    DeviceTests.collection = db.device_test
     unittest.main()
 
 if __name__ == "__main__":
