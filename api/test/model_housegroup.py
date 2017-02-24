1c185f982c3852acba2256ebf8acc52e98565a55
Benny Clark <Benny Clark>
2017-02-24 13:06:03 +0000

    more unit tests and CI setup

--- api/test/model_housegroup.py
+++ api/test/model_housegroup.py
@@ -19,16 +19,6 @@ class HouseGroupTests(unittest.TestCase):
         name = house_group.get_house_group_name()
         self.assertEqual(name, "Redland Houses", "House Group name not added correctly.")
 
-    def test_GetRoomsForHouse(self):
-        houses = self.houses.get_houses_for_house(self.house1id)
-        self.assertEqual(len(houses), 3, "Incorrect amount of rooms.")
-        name1 = houses[0].get_house_name()
-        name2 = houses[1].get_house_name()
-        name3 = houses[2].get_house_name()
-        self.assertEqual(name1, "Living Room", "First room has incorrect name.")
-        self.assertEqual(name2, "Kitchen", "Second room has incorrect name.")
-        self.assertEqual(name3, "Bathroom", "Third room has incorrect name.")
-
     def test_HousesAddedCorrectly(self):
         self.house_groups.add_house_to_group(self.house1id, self.housegroup_id)
         house_group = self.house_groups.get_house_group_by_id(self.housegroup_id)
@@ -40,3 +30,13 @@ class HouseGroupTests(unittest.TestCase):
         all_remaining_house_groups = self.house_groups.get_all_house_groups()
         self.assertEqual(len(all_remaining_house_groups), len(all_house_groups) - 1,
                          "Incorrect number of remaining house groups.")
+
+    def test_GetRoomsForHouse(self):
+        houses = self.houses.get_houses_for_house(self.house1id)
+        self.assertEqual(len(houses), 3, "Incorrect amount of rooms.")
+        name1 = houses[0].get_house_name()
+        name2 = houses[1].get_house_name()
+        name3 = houses[2].get_house_name()
+        self.assertEqual(name1, "Living Room", "First room has incorrect name.")
+        self.assertEqual(name2, "Kitchen", "Second room has incorrect name.")
+        self.assertEqual(name3, "Bathroom", "Third room has incorrect name.")
