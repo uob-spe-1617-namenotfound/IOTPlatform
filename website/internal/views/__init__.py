c52b12ec0b208078259ee57fcda5aad6743e7f14
David <dz15376@my.bristol.ac.uk>
2017-04-02 23:43:59 +0100

    graph working with bootstrap elements

--- website/internal/views/__init__.py
+++ website/internal/views/__init__.py
@@ -49,4 +49,4 @@ def themes():
 @internal_site.route("/graph")
 def graph():
     data = data_interface.get_user_graph_data(utilities.session.get_active_user()['user_id'])
-    return render_template("internal/graph.html", data=data)
+    return render_template("internal/graph.html")
