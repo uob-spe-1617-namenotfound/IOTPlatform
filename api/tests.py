016b510763a29d9df0450b4519a5d0da07056ee0
Benny Clark <Benny Clark>
2017-04-15 21:14:53 +0100

    Theme functionality with tests

--- api/tests.py
+++ api/tests.py
@@ -11,6 +11,7 @@ from test.model_house import HouseTests
 from test.model_room import RoomTests
 from test.model_device import DeviceTests
 from test.model_trigger import TriggerTests
+from test.model_theme import ThemeTests
 from test.model_usr_mgmt import MgmtTests
 from test.model_token import TokenTests
 from test.model_user import UserTests
@@ -29,6 +30,7 @@ def main():
     RoomTests.repository_collection = repository_collection
     DeviceTests.repository_collection = repository_collection
     TriggerTests.repository_collection = repository_collection
+    ThemeTests.repository_collection = repository_collection
     TokenTests.repository_collection = repository_collection
     AdminTests.repository_collection = repository_collection
     MgmtTests.repository_collection = repository_collection
